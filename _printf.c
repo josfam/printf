d723c6596aa6cc0a11d4bfecf1fec9a0fb759595
josfam <josephamukun@tutanota.com>
2023-11-10 19:19:55 +0300

    Add code for detecting the `%c` format specifier.
    The `%c` format specifier is now handled in a new `print_special`
    function.
    An if-else is now used for the control flow.

--- _printf.c
+++ _printf.c
@@ -13,17 +13,57 @@ int _printf(const char *format, ...)
 {
 	int charCount; /* Characters printed */
 	const char *charPtr;
+	va_list arguments;
 
 	charCount = 0;
 	charPtr = format;
 
-	/* Naively print every character that is encountered */
+	va_start(arguments, format);
+
+	/* Print a character, while checking for directives */
 	while (*charPtr != '\0')
 	{
-		_putchar(*charPtr);
-		charCount++;
-		charPtr++;
+		if (*charPtr != '%')
+		{
+			_putchar(*charPtr);
+			charCount++;
+			charPtr++;
+		}
+		else /* Print the character based on what's after the % */
+		{
+			print_special(charPtr, arguments, &charCount);
+			charPtr += 2; /* Skip the symbol after the % sign */
+		}
 	}
+	va_end(arguments);
 
 	return (charCount);
+}
+
+/**
+ * print_special - Prints a character, based on the directive found after the
+ *                 % sign.
+ * @charPtr: Pointer to the current position in the format string being
+ *           investigated.
+ * @arguments: The variable arguments list.
+ * @charCount: Pointer to the number of characters printed so far.
+ * Description: Prints a character, based on the directive found after the
+ *              % sign.
+ * Return: 0 on success.
+ */
+int print_special(const char *charPtr, va_list args, int *charCount)
+{
+	char next_char = *(charPtr + 1); /* char after the % */
+
+	switch (next_char)
+	{
+		case 'c':
+			_putchar(va_arg(args, int));
+			(*charCount)++;
+			break;
+		default:
+			_putchar(*charPtr);
+			(*charCount)++;
+	}
+	return (0);
 }
--- main.h
+++ main.h
@@ -1,10 +1,14 @@
 #ifndef _PRINTF_HEADER
 #define _PRINTF_HEADER
+#include <stdarg.h>
 
 /* Writes a character to standard output. */
 void _putchar(char ch);
 
 /* Produces output according to a format */
 int _printf(const char *format, ...);
+
+/* Prints a character, based on the directive found after the % sign. */
+int print_special(const char *charPtr, va_list arguments, int *charCount);
 
 #endif
