8478bd71d8265b95b380066c89f8fdf8fd77bba5
josfam <josephamukun@tutanota.com>
2023-11-12 20:40:49 +0300

    Add buffer-related functions in a new file

---
+++ _buffer.c
@@ -0,0 +1,65 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_buffer - Creates a buffer to be used for printing.
+ * Return: Pointer to the created buffer.
+*/
+charBuffer *create_buffer()
+{
+	charBuffer *buffer;
+	buffer = (charBuffer*)malloc(sizeof(charBuffer));
+
+	buffer->bufferPtr = buffer->chars; /* setup the pointer to the char array */
+	return (buffer);
+}
+
+/**
+ * flush_buffer - Writes all the buffer's contents to standard out, and
+ *                effectively "resets" the buffer, such that it can accept more
+ *                characters.
+ * @buffer: The buffer to be flushed.
+ * Return: Nothing.
+*/
+void flush_buffer(charBuffer *buffer)
+{
+	char *start;
+	int step;
+
+	start = buffer->chars;
+	step = 0;
+
+	/* fill the buffer with all '0's */
+	while(step <= BUFFER_SIZE)
+	{
+		buffer->chars[step] = '0';
+		start++;
+		step++;
+	}
+
+	/* reset buffer pointers */
+	buffer->bufferPtr = buffer->chars;
+}
+
+/**
+ * buffer_almost_full - Returns whether the buffer is almost full.
+ * @buffer: The buffer to be checked.
+ * Return: Whether the buffer is considered full.
+*/
+int buffer_almost_full(charBuffer *buffer)
+{
+	int leftOverSpace;
+
+	leftOverSpace = (buffer->chars + BUFFER_SIZE) - buffer->bufferPtr;
+	return (leftOverSpace <= THRESHOLD_FOR_FULL);
+}
+
+/**
+ * free_buffer_memory - Frees the memory requested by a charBuffer
+ * @buffer: The charBuffer whose memory to free.
+ * Return: Nothing.
+*/
+void free_buffer_memory(charBuffer *buffer)
+{
+	free(buffer);
+}
