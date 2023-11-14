12e59cca243c960d7913461da4016f10de25d5be
josfam <josephamukun@tutanota.com>
2023-11-14 09:09:46 +0300

    Free requested buffer memory in _show_binary

--- _show_binary.c
+++ _show_binary.c
@@ -37,6 +37,8 @@ int show_binary(va_list args, int *charCount, charBuffer *buffer)
 	{
 		append_to_buffer('0' + binary_nums[j], buffer, charCount);
 	}
-	
+
+	free_buffer_memory(buffer);
+
 	return (0);
 }
