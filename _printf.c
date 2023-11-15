de7d9fc9dba45d065f488c9334d4f548100faa38
Opeyemi Osakuade <osakuadeopeyemi@gmail.com>
2023-11-14 19:19:44 -0800

    replaced spaces with tab

--- _printf.c
+++ _printf.c
@@ -28,7 +28,7 @@ int _printf(const char *format, ...)
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
 	if (format[0]  == '%' && format[1] == '\0')
-                return (-1);
+		return (-1);
 
 	/* Print a character, while checking for directives */
 	while (*charPtr != '\0')
